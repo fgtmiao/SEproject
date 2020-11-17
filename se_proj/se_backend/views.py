fba6b818bc27bedc0f361a02fc89018fedf27130
yellow-binary-tree <wangyueqian@pku.edu.cn>
2020-11-17 22:27:52 +0800

    init pages & add database settings

--- se_proj/se_backend/views.py
+++ se_proj/se_backend/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse('Hello World from page index!')
+
+def userinfo(request):
+    return HttpResponse('Hello World from page userinfo!')
+
+def baike_content(request):
+    return HttpResponse('Hello World from page baike_content!')
