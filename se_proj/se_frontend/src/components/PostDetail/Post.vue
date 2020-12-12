ade72f9b91b55d76da8e7dc2584914dfc0724746
fgtmiao <1922903760@qq.com>
2020-12-12 17:10:57 +0800

    删除了部分测试

--- se_proj/se_frontend/src/components/PostDetail/Post.vue
+++ se_proj/se_frontend/src/components/PostDetail/Post.vue
@@ -227,42 +227,6 @@ export default {
                         }
                     ]
                 },
-                {
-                    name:'Taylor Swift',
-                    id:19891221,
-                    headImg:'https://ae01.alicdn.com/kf/H94c78935ffa64e7e977544d19ecebf06L.jpg',
-                    comment:'我发行了我的新专辑Lover',
-                    time:'2019年9月16日 18:43',
-                    commentNum:1,
-                    like:5,
-                    inputShow:false,
-                    reply:[
-                        {
-                            from:'Lana Del Rey',
-                            fromId:19870621,
-                            fromHeadImg:'https://ae01.alicdn.com/kf/Hd60a3f7c06fd47ae85624badd32ce54dv.jpg',
-                            to:'Taylor Swift',
-                            toId:19891221,
-                            comment:'新专辑和speak now 一样棒！',
-                            time:'2019年9月16日 18:43',
-                            commentNum:25,
-                            like:5,
-                            inputShow:false
- 
-                        }
-                    ]
-                },
-                {
-                    name:'Norman Fucking Rockwell',
-                    id:20190830,
-                    headImg:'https://ae01.alicdn.com/kf/Hdd856ae4c81545d2b51fa0c209f7aa28Z.jpg',
-                    comment:'Plz buy Norman Fucking Rockwell on everywhere',
-                    time:'2019年9月16日 18:43',
-                    commentNum:0,
-                    like:5,
-                    inputShow:false,
-                    reply:[]
-                },
             ]
         }
     },
